8bf53d84f18beaa55a97f33baef8dcc4a4e7e865
dashath0097 <dashathk0440@gmail.com>
2025-02-20 12:43:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,19 +10,18 @@ terraform {
 provider "spacelift" {
   # Spacelift API credentials (configure via environment variables)
 }
-# Fetch all stacks dynamically
 data "spacelift_stacks" "all_stacks" {}
 
 # Define the policy
 resource "spacelift_policy" "iam_policy_approval" {
-  name       = "require_security_approval_for_iam_policy"
-  type       = "PLAN"
-  body       = file("iam_policy_approval.rego")
+  name = "require_security_approval_for_iam_policy"
+  type = "PLAN"
+  body = file("iam_policy_approval.rego")
 }
 
 # Attach policy to all stacks
 resource "spacelift_policy_attachment" "iam_policy_attachment" {
-  for_each  = { for stack in data.spacelift_stacks.all_stacks.stacks : stack.id => stack }
+  for_each  = toset(data.spacelift_stacks.all_stacks.ids)
 
   stack_id  = each.key
   policy_id = spacelift_policy.iam_policy_approval.id
